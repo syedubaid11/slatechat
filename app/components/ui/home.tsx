61d47fdec55f0e7365ade779e2d9f0c7d8efeef6
syedubaid <syedabuubaid@gmail.com>
2024-09-11 23:12:14 +0530

    commit

--- app/components/ui/home.tsx
+++ app/components/ui/home.tsx
@@ -2,8 +2,8 @@ export const HomeHero=()=>{
     return(
         <>
         <div className="flex flex-row justify-center bg-black">
-            <div className="text-6xl font-bold font-white">
-                slate chat
+            <div className="text-6xl font-bold flex">
+                <p className="text-red-600">s</p><p className="text-yellow-600">l</p><p className="text-green-600">a</p><p className="text-blue-600">t</p><p className="text-pink-600">e</p>chat
             </div>
         </div>
         </>
