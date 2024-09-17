112fd28cf1386b955fb2d2e9e02ef1bacef79682
syedubaid <syedabuubaid@gmail.com>
2024-09-18 01:44:39 +0530

    commit

--- server.js
+++ server.js
@@ -1,34 +1,47 @@
-import WebSocket, { WebSocketServer } from 'ws';
-import http from 'http';
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 
-//create  a simple http server
-const server = http.createServer(function(request,response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.end("hi there");
+// Create an Express application
+const app = express();
+const port = 8080;
+
+// Middleware to log request details
+app.use((req, res, next) => {
+    console.log((new Date()) + ' Received request for ' + req.url);
+    next(); // Pass control to the next middleware function
 });
 
-//all the websocket logic starts here
-const wss = new WebSocketServer({ server });
+// Route handler for the root path
+app.get('/', (req, res) => {
+    res.send("hi there");
+});
 
-let usercount=0
+// Create an HTTP server and pass the Express app to it
+const server = http.createServer(app);
+
+// Initialize WebSocket server
+const wss = new WebSocket.Server({ server });
+
+let usercount = 0;
 
 wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+    ws.on('error', console.error);
 
-  ws.on('message', function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
+    ws.on('message', function message(data, isBinary) {
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        });
     });
-  });
 
-  ws.send('Hello! Message From Server!!');
-  ++usercount;
-  console.log('user connected',usercount)
-
+    ws.send('Hello! Message From Server!!');
+    ++usercount;
+    console.log('user connected', usercount);
 });
 
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
+// Start the server
+server.listen(port, () => {
+    console.log((new Date()) + ' Server is listening on port ' + port);
 });
