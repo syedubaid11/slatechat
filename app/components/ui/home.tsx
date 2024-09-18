78be1626b414072b2f8deda6e9142be5c6aa153f
syedubaid <syedabuubaid@gmail.com>
2024-09-18 16:30:14 +0530

    commit

--- app/components/ui/home.tsx
+++ app/components/ui/home.tsx
@@ -3,11 +3,10 @@ export const HomeHero=()=>{
         <>
         <div className="font-londrina flex flex-row items-center justify-center">
             <div className="text-8xl font-bold flex">
-                <p className="text-red-600">s</p><p className="text-yellow-400">l</p><p className="text-green-600">a</p><p className="text-blue-600">t</p><p className="text-pink-600">e</p>
+                <p className="text-red-600">s</p><p className="text-yellow-400">l</p><p className="text-green-600">a</p><p className="text-blue-600">t</p><p className="text-pink-600">e</p><p className="font-londrina text-8xl ml-2">chat</p>
+
             </div>
-            <div className="mt-2">
-              <p className="font-londrina text-6xl ml-2">chat</p>
-            </div>
+            
         </div>
         </>
     )
