4a7c4765a67d4eb472e7bc11174f8bcdcb7e6e86
syedubaid <syedabuubaid@gmail.com>
2024-09-19 21:51:18 +0530

    commit

--- app/(api)/auth/[...nextauth]/route.ts
+++ app/(api)/auth/[...nextauth]/route.ts
@@ -1,5 +1,25 @@
-import { NextRequest , NextResponse } from "next/server";
+import NextAuth from "next-auth"
+import CredentialsProvider from 'next-auth/providers/credentials'
 
-export function GET(req:NextRequest){
-    return NextResponse.json({message:"Handler"})
-}
+const handler= NextAuth({
+    providers:[
+        CredentialsProvider({
+            name:"Email",
+            credentials:{
+                username:{label:'email',type:"text",placeholder:'Email'},
+                password:{label:'password',type:"text",placeholder:"Password"},
+            },
+            async authorize(credentials:any){
+                console.log(credentials)
+                return {
+                    id:"user1",
+                    name:"ubaid",
+                    email:"syedd@gmail.com"
+                }
+            }
+        })
+    ]
+});
+
+export const GET=handler;
+export const POST=handler;
