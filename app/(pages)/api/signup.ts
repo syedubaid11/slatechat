dd8adc9d2529e7b412837f9569d1b945eb719418
syedubaid <syedabuubaid@gmail.com>
2024-09-24 02:28:11 +0530

    commit

---
+++ app/(pages)/api/signup.ts
@@ -0,0 +1,58 @@
+import { lucia } from "@/lib/auth";
+import { generateIdFromEntropySize } from "lucia";
+import { hash } from "@node-rs/argon2";
+import { Prisma } from "@prisma/client";
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	if (req.method !== "POST") {
+		res.status(404).end();
+		return;
+	}
+
+	const body: null | Partial<{ username: string; password: string }> = req.body;
+	const username = body?.username;
+	if (
+		!username ||
+		username.length < 3 ||
+		username.length > 31 ||
+		!/^[a-z0-9_-]+$/.test(username)
+	) {
+		res.status(400).json({
+			error: "Invalid username"
+		});
+		return;
+	}
+	const password = body?.password;
+	if (!password || password.length < 6 || password.length > 255) {
+		res.status(400).json({
+			error: "Invalid password"
+		});
+		return;
+	}
+
+	const passwordHash = await hash(password, {
+		// recommended minimum parameters
+		memoryCost: 19456,
+		timeCost: 2,
+		outputLen: 32,
+		parallelism: 1
+	});
+	const userId = generateIdFromEntropySize(10); // 16 characters long
+
+	// TODO: check if username is already used
+	await prisma.user.create({
+        data: {
+          id: userId,
+          username: username,
+          password_hash: passwordHash,
+        },
+      })
+
+	const session = await lucia.createSession(userId, {});
+	res.appendHeader("Set-Cookie", lucia.createSessionCookie(session.id).serialize())
+		.status(200)
+		.end();
+}
---
+++ app/(pages)/signup.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "next/router";
+import type { FormEvent } from "react";
+
+export default function Page() {
+	const router = useRouter();
+
+	async function onSubmit(e: FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		const formElement = e.target as HTMLFormElement;
+		const response = await fetch(formElement.action, {
+			method: formElement.method,
+			body: JSON.stringify(Object.fromEntries(new FormData(formElement).entries())),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+		if (response.ok) {
+			router.push("/");
+		}
+	}
+
+	return (
+		<>
+			<h1>Create an account</h1>
+			<form method="post" action="/api/signup" onSubmit={onSubmit}>
+				<label htmlFor="username">Username</label>
+				<input name="username" id="username" />
+				<br />
+				<label htmlFor="password">Password</label>
+				<input type="password" name="password" id="password" />
+				<br />
+				<button>Continue</button>
+			</form>
+		</>
+	);
+}
