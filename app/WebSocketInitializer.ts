347984d5732bb3cbe9a645d2c2fd2b49ca81f0a1
syedubaid <syedabuubaid@gmail.com>
2024-09-27 12:11:25 +0530

    commit

--- app/WebSocketInitializer.ts
+++ app/WebSocketInitializer.ts
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 export default function WebSocketInitializer() {
   useEffect(() => {
     const connectToWebSocket = async () => {
-      await fetch('/api/websocket'); // Initialize WebSocket server
+      await fetch('/api/socket'); // Initialize WebSocket server
       console.log("Connection established")
     };
     connectToWebSocket();
