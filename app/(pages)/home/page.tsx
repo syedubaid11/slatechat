d8ecf0500d3a75f793fad3d69f6aeb9cdb2fe3c3
syedubaid <syedabuubaid@gmail.com>
2024-10-15 23:59:01 +0530

    commit

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -2,10 +2,12 @@
 import { Button } from "@/components/ui/arrow"
 import { Righteous } from "next/font/google"
 import { Londrina_Sketch } from "next/font/google"
-import { Info } from "@/components/ui/info"
 import { ChatSvg } from "@/components/ui/chatsvg"
 import { PenSvg } from "@/components/ui/pensvg"
 import { PersonSvg } from "@/components/ui/personsvg"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation"
+
 
 const righteous=Righteous({
     weight:'400',
@@ -18,9 +20,8 @@ const londrina=Londrina_Sketch({
     
 })
 
-
-
 export default function Home(){
+    const router=useRouter();
     return(
         
         <div className="flex flex-col ">
@@ -35,25 +36,35 @@ export default function Home(){
                             <div>Contact</div>
                         </div>
                 </div>
-                <div className="flex flex-col items-center mt-10">
-                    <h1 className="text-2xl text-strong">Chat and Draw Together</h1>
-                    Experience real-time collaboration like never before with SlateChat. Connect, communicate, and create in one seamless platform.
-                    <Button/>
-                    
 
+                <div className={`${righteous.className} flex flex-col items-center mt-40`}>
+                    <h1 className="text-4xl text-strong m-4">Chat and Draw Together</h1>
+                    <p className="text-xl mb-4">Experience real-time collaboration like never before with SlateChat. Connect, communicate, and create in one seamless platform.</p>
+                    <Button onClick={()=>{router.push('/canvas')}}/>
                 </div>
             </div>
 
             <div className={`${londrina.className} text-4xl h-80 m-20 w-auto border border-black rounded-2xl`}>
-                Features
-                <div className="flex flex-row justify-center border h-60">
-                    <div className="border w-60 mr-20">
+                <h1 className="ml-6">Features</h1>
+                <div className="flex flex-row justify-center h-60">
+                    <div className="w-60 mr-20">
                         <div><ChatSvg/></div>
-                        <div><p className="text-2xl">Communicate instantly with your team members through our lightning-fast chat system.</p></div>
+                        <div><p className={`${righteous.className} text-xl`}>Communicate instantly with your team members through our lightning-fast chat system.</p></div>
                         
                     </div>
-                    <div className="border w-60 mr-20"><PenSvg/></div>
-                    <div className="border w-60 mr-20"><PersonSvg/></div>
+                    <div className=" w-60 mr-20">
+                        <div><PenSvg/></div>
+                        <div>
+                        <p className={`${righteous.className} text-xl`}>Bring your ideas to life with our shared canvas. Draw, sketch, and create together in real-time.</p></div>
+                        </div>
+                    <div className=" w-60 mr-20">
+                        <div>
+                           <PersonSvg/>
+                        </div>
+                        <div>
+                           <p className={`${righteous.className} text-xl`}>Dedicated workspaces for seamless collaboration</p>
+                        </div>
+                        </div>
                 </div>
 
 
--- components/ui/arrow.tsx
+++ components/ui/arrow.tsx
@@ -1,15 +1,16 @@
 
-export const Button=()=>{
-    return(
-        <button type="button" className="text-white bg-black hover:bg-neutral-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+interface ButtonProps {
+    onClick: () => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({ onClick }) => {
+    return (
+        <button onClick={onClick} type="button" className="text-white bg-black hover:bg-neutral-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             <div className="mr-2">Get Started</div>
             <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
             </svg>
-            
             <span className="sr-only">Icon description</span>
-            
         </button>
-    )
-
+    );
 }
