a0ffcb6159cc844a07330ac6ee976ac5490b7d52
syedubaid <syedabuubaid@gmail.com>
2024-12-25 01:48:42 +0530

    commit

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,28 +1,69 @@
+import { useState } from 'react';
 import './component.css';
 
 export const Navbar: React.FC = () => {
+    const [isOpen,setisOpen]=useState(false);
+
+    const toggleClick=()=>{
+        setisOpen(!isOpen)
+    }
     return (
-        <>
-        <div className="w-full h-[80px] backdrop-blur-md flex space-between gap-[5px] bg-black text-white items-center">
-            <div className="ml-[10px] fadein">
-                slatechat
+        
+        <div className='relative'>
+
+        <div className="absolute w-full h-max bg-black text-white items-center z-20">
+
+            <div className='flex flex-row px-3 py-5 items-center'>
+
+                <div onClick={toggleClick} className="desktop:hidden ml-[10px] fadein">
+                    <div className={`w-[30px] h-[1px] bg-white transition-transform duration-300 rounded-lg ${isOpen?'rotate-45 translate-y-1.5':''}`}></div>
+                    <div className={`w-[30px] h-[1px] bg-white my-1 transition-all duration-300 rounded-lg opacity-100 ${isOpen?'opacity-0':'opacity-1'}`}></div>
+                    <div className={`w-[16px] h-[1px] bg-white transition-transform duration-300 rounded-lg ${isOpen?'w-[30px] -rotate-45 -translate-2':''}`}></div>
+                </div>
+
+                <div className="ml-[10px] fadein ">
+                    slatechat
+                </div>
             </div>
-            <div className="ml-[20px] fadein">
+        
+
+        <div className='flex space-between gap-[5px]'>
+
+            <div className="ml-[20px] fadein hidden">
                 about
             </div>
-            <div className='fadein'>
+            <div className='fadein hidden'>
                 features
             </div>
-            <div className='fadein'>
+            <div className='fadein hidden'>
                 contribute
             </div>
-            <div className='fadein'>
+            <div className='fadein hidden'>
                 github link
             </div>
         </div>
+           
+      
+        </div>
+        {isOpen&&
+        <div className='flex flex-col slide-right absolute h-screen w-screen backdrop-blur-md top-0 z-10 bg-black/80 text-white bg-transparent'>
+            <div>
+            <div className="mt-[90px]"></div>
 
+                <div className='text-4xl'>Navigate</div>
+                <div>das</div>
+                <div>das</div>
+                <div>das</div>
+                <div>dasd</div>
+                <div>das</div>
+             </div>   
+            
+        </div>
+}
+        
+        
 
         
-        </>
+        </div>
     );
 }
