6e1b487fd6af9b231e5b60afccf67269c9245212
syedubaid <syedabuubaid@gmail.com>
2024-12-24 23:52:21 +0530

    commit

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 
+
+
+
 export async function GET(){
     return NextResponse.json({message:"You hit this endpoint"})
 }
--- app/api/user/signup/route.ts
+++ app/api/user/signup/route.ts
@@ -1,7 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const signup=z.object({
+    username:z.string(),
+    password:z.string()
+})
 
 
 export async function POST(req:NextRequest){
+    console.log('')
     return NextResponse.json({message:"you hit signup route"})
 }
 
