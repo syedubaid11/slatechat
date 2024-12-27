0768d9527b153ded9652a6da12a3c7e1ec459cb8
syedubaid <syedabuubaid@gmail.com>
2024-12-27 16:39:06 +0530

    commit

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+     //@ts-ignore
+      clientId: process.env.GITHUB_ID,
+      //@ts-ignore
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+}
