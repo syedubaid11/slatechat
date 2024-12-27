98946060d5b2146b70308da40266cf4c69333c03
syedubaid <syedabuubaid@gmail.com>
2024-12-27 20:08:37 +0530

    commit

---
+++ app/api/user/info/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest,NextResponse } from "next/server";
+
+
+export async function GET(req:NextRequest) {
+    //verify if the user is logged in or not 
+
+
+    
+}
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from "next/server";
 
-
-
-
 export async function GET(){
     return NextResponse.json({message:"You hit this endpoint"})
 }
+
 
 
 
