567e25059719ed2ebc0f1dbfed6525f41445a0a9
syedubaid <syedabuubaid@gmail.com>
2025-01-09 02:51:35 +0530

    commit

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -1,8 +1,8 @@
 export const Hero=()=>{
     return(
-        <div className="flex flex-col">
+        <div className="flex flex-col mt-20 items-center">
             <div className="">Discover seamless and fast experience</div>
-            <div className="flex flex-col">
+            <div className="flex flex-col mt-[70px]">
                 <div>
                     Safe authenticaition
                 </div>
@@ -10,7 +10,7 @@ export const Hero=()=>{
                     Connect with other users
                 </div>
             </div>
-             
+
         </div>
     )
 }
