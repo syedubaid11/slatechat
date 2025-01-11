163a8d29340ddea6ea7c6eb78afd689db8fc79a8
syedubaid <syedabuubaid@gmail.com>
2025-01-11 19:19:28 +0530

    commit

--- components/ui/cards.tsx
+++ components/ui/cards.tsx
@@ -1,6 +1,14 @@
-export const Cards=(props: any)=>{
+interface propsinterface{
+    svg:React.ReactNode,
+    title:string
+}
+
+export const Cards=(props: propsinterface)=>{
     return (
-        <div className="border h-[150px] w-[340px] border-gray-700 rounded-xl mt-[10px] bg-neutral-850">
+        <div className="border h-[150px] w-[340px] border-gray-700 rounded-xl mt-[10px] bg-neutral-850 flex flex-row">
+            <div className="p-[10px]">
+               {props.svg}
+            </div>
             <div className="p-[10px]">
                 {props.title}
             </div>
