5eac28c407cd0036b9b69ba38279cee06799e3b4
installer <installer@gmail.com>
2025-01-14 20:51:12 +0900

    commit

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -3,7 +3,7 @@ import { Cards } from "./cards"
 export const Hero=()=>{
     return(
         <div className="flex flex-col mt-20 items-center mb-20">
-            <div className="text-[23px] bg-gradient-to-r from-gray-50 to-indigo-600 bg-clip-text text-transparent">Seamless and Minimal Experience</div>
+            <div className="text-[21px] bg-gradient-to-r from-gray-50 to-indigo-600 bg-clip-text text-transparent">Seamless and Minimal Experience</div>
             <div className="flex flex-col mt-[5px] px-[20px] justify-center items-center">
                 <div className="text-white tracking-wide">
                     Safe authenticaition along with customised profiles , connect with other users seamlessly
