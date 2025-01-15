fd08392f853020cf472c070dfb1fc7cfac601896
installer <installer@gmail.com>
2025-01-15 19:43:06 +0530

    commit

---
+++ app/api/user/friends/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse,NextRequest } from "next/server";
+import { PrismaClient } from '@prisma/client'
+
+const prisma=new PrismaClient()
+
+export async function GET(req:NextRequest){
+    
+    
+}
