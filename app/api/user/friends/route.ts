1a8a020b622f525d560e1e77f6c7c2886b996440
installer <installer@gmail.com>
2025-01-16 16:28:12 +0530

    commit

--- app/api/user/friends/route.ts
+++ app/api/user/friends/route.ts
@@ -3,7 +3,13 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma=new PrismaClient()
 
-export async function GET(req:NextRequest){
-    
-    
+export async function POST(req:NextRequest,userId:number,friendId:number){
+    const friendRequest=await prisma.friend.create({
+        data:{
+            userId,
+            friendId,
+            status:'Pending'
+        }
+
+    })
 }
