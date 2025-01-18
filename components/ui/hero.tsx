b959edd21542980f9466166f9595f6e44dc0c569
installer <installer@gmail.com>
2025-01-19 00:52:21 +0530

    commit

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -1,22 +1,24 @@
+import Link from "next/link"
 import { Cards } from "./cards"
-import { useRouter } from "next/router"
 
 
 export const Hero=()=>{
-    const router=useRouter();
+    
     return(
         <div className="flex flex-col mt-20 items-center mb-20">
             <div className="text-[21px] bg-gradient-to-r from-gray-50 to-indigo-600 bg-clip-text text-transparent">Seamless and Minimal Experience</div>
             <div className="flex flex-col mt-[5px] px-[20px] justify-center items-center">
+
                 <div className="text-white tracking-wide">
                     Safe authenticaition along with customised profiles , connect with other users seamlessly
                 </div>
                 <div>
                 </div>
+
             </div>
             <div className="h-[150px] w-max flex items-center justify-center gap-[10px]">
                     <div className="text-white tracking-wide">
-                        <button className="border border-gray-800 p-[10px] rounded-xl">Get Started</button>
+                        <Link href="/canvas"> <button  className="border border-gray-800 p-[10px] rounded-xl">Get Started</button></Link>
                     </div>
                     <div className="text-white tracking-wide">
                      <button className="border border-gray-800 p-[10px] rounded-xl">Contribute</button>
