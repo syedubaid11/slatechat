f987c608ab8f166a67a4f85a5228ebb7b6e097aa
installer <syedabuubaid@gmail.com>
2025-01-26 14:37:10 +0530

    commit

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -5,9 +5,14 @@ import { useUser } from "@clerk/nextjs";
 export const FloatingNav=()=>{
   const {user}=useUser();
   console.log(user?.fullName)
+  console.log(user?.imageUrl)
 
   return(
-    <div className="absolute top-[10px] height-[100px] p-[15px] rounded-xl w-[250px] backdrop-blur-1 bg-transparent border flex flex-row gap-[20px]">
+    <div className="absolute top-[10px] height-[100px] p-[15px] rounded-xl w-max backdrop-blur-1 bg-transparent border flex flex-row gap-[20px] justify-center items-center">
+      <div>
+        <img src={user?.imageUrl} className="w-[22px] h-[22px] rounded-full" />
+
+      </div>
       <Link href="/">
         <div className="hover:cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
